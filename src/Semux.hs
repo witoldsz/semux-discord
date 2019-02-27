3fbfc1ed34c108a1690dd6d604dda84e1428af3e
Witold Szczerba <witoldsz.dev@gmail.com>
2019-02-27 22:49:23 +0100

    use Network.HTTP.Simple for Semux API

--- src/Semux.hs
+++ src/Semux.hs
@@ -10,42 +10,37 @@ import Data.Time
 import Data.Maybe
 import Data.Aeson
 import GHC.Generics (Generic)
-import qualified Data.ByteString.Lazy as B
-import Network.HTTP.Conduit (simpleHttp)
+import Network.HTTP.Simple
 
 pageSize :: Int
 pageSize = 100
 
 getLastCoinbase :: String -> String -> IO UTCTime
 getLastCoinbase semuxApi delegate = do
-  account <- getRight $ getAccount semuxApi delegate
+  account <- getAccount semuxApi delegate
   let txCount = (transactionCount . result) account
   tx <- findLastCoinbase txCount
   print tx
   return $ timestamp tx
 
   where
-  findLastCoinbase :: Int -> IO Transaction
-  findLastCoinbase lastTx = do
-    response <- getRight $ getTransactions semuxApi delegate txRange
-    let txsRev = (Data.List.reverse . result) response
-    let lastCoinbase = Data.List.find (\i -> transactionType i == "COINBASE") txsRev
-    solution lastCoinbase
-    where
-      from = max 0 (lastTx - pageSize)
-      txRange = (from, lastTx)
+    findLastCoinbase :: Int -> IO Transaction
+    findLastCoinbase lastTx = do
+      response <- getTransactions semuxApi delegate txRange
+      let txsRev = (Data.List.reverse . result) response
+      let lastCoinbase = Data.List.find (\i -> transactionType i == "COINBASE") txsRev
+      solution lastCoinbase
+      where
+        from = max 0 (lastTx - pageSize)
+        txRange = (from, lastTx)
 
-      solution :: Maybe Transaction -> IO Transaction
-      solution (Just coinbase ) =
-        return coinbase
-      solution Nothing =
-        if (from > 0)
-          then findLastCoinbase from
-          else fail "No COINBASE found"
-
-getRight :: IO (Either String a) -> IO a
-getRight x =
-  x >>= either fail return
+        solution :: Maybe Transaction -> IO Transaction
+        solution (Just coinbase ) =
+          return coinbase
+        solution Nothing =
+          if from > 0
+            then findLastCoinbase from
+            else fail "No COINBASE found"
 
 -- Account
 data Account = Account
@@ -54,15 +49,11 @@ data Account = Account
 
 instance FromJSON Account
 
-getAccount :: String -> String -> IO (Either String (Response Account))
+getAccount :: String -> String -> IO (ApiResponse Account)
 getAccount semuxApi addr =
-  eitherDecode <$> fetchJson
+  getResponseBody <$> httpJSON (parseRequest_ url)
   where
-    fetchJson :: IO B.ByteString
-    fetchJson =
-      putStrLn url >> simpleHttp url
-      where
-        url = semuxApi ++ "account?address=" ++ addr
+    url = semuxApi ++ "account?address=" ++ addr
 
 -- Transaction
 data Transaction = Transaction
@@ -75,27 +66,23 @@ instance FromJSON Transaction where
     <$> fmap textToUTC (v .: "timestamp")
     <*> v .: "type"
 
-getTransactions :: String -> String -> (Int, Int) -> IO (Either String (Response [Transaction]))
+getTransactions :: String -> String -> (Int, Int) -> IO (ApiResponse [Transaction])
 getTransactions semuxApi addr (from, to) =
-  eitherDecode <$> fetchJson
+  getResponseBody <$> httpJSON (parseRequest_ url)
   where
-    fetchJson :: IO B.ByteString
-    fetchJson =
-      putStrLn url >> simpleHttp url
-      where
-        url =
-          semuxApi ++ "account/transactions?address=" ++ addr ++ "&from=" ++ (show from) ++ "&to=" ++ (show to)
+    url =
+      semuxApi ++ "account/transactions?address=" ++ addr ++ "&from=" ++ show from ++ "&to=" ++ show to
 
 -- Response
-data Response a = Response
-  { success :: Bool
-  , message :: !Text
-  , result :: a
-  } deriving (Show, Generic)
+data ApiResponse a = Response
+     { success :: Bool
+     , message :: !Text
+     , result :: a
+     } deriving (Show, Generic)
 
-instance FromJSON (Response [Transaction])
-instance FromJSON (Response Account)
+instance FromJSON (ApiResponse [Transaction])
+instance FromJSON (ApiResponse Account)
 
 -- UTCTime
 textToUTC :: Text -> UTCTime
-textToUTC = fromJust . (parseTimeM False defaultTimeLocale "%s") . unpack . (Data.Text.take 10)
+textToUTC = fromJust . parseTimeM False defaultTimeLocale "%s" . unpack . Data.Text.take 10
