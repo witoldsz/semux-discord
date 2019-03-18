d6efce5ff0629ccd59b6096474ad80cadff09d26
Witold Szczerba <witoldsz.dev@gmail.com>
2019-03-18 20:40:23 +0100

    full feat bot, wip… day: 14

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards   #-}
 
 module Main where
 
 import App.App
 import System.Environment
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr NoBuffering
+
   semuxApiUrl <- getEnv "SEMUX_API"
   dicordSecret <- getEnv "DISCORD_SECRET"
+
   app Configuration{..}
